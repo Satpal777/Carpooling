3438d4f1fe85886396adb6f3bac9630749d927db
Satpal777 <85938637+Satpal777@users.noreply.github.com>
2021-09-27 09:02:57 +0530

    Update and rename index.php to login.php

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         session_start();
         $_SESSION['loggedin'] = true;
         $_SESSION['email'] = $email;
-        header("location: home.php");
+        header("location: /index.php");
       }
     }
 
