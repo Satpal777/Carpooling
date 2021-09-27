81cb25b1b77c5fddc2541f74ab24bd6b7cdfee49
Satpal777 <85938637+Satpal777@users.noreply.github.com>
2021-09-27 09:03:38 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var Api = "https://localhost:4200/api/contacts";
- 
-Unirest.post(postApi)
-        .header("accept", "application/json")
-        .header("Content-Type", "application/json")
-        .body(template.render(model))
-        .asJson();
