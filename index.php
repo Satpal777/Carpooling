594fa365645f3cf76880f1594d8335f9fa48f5bb
Satpal777 <85938637+Satpal777@users.noreply.github.com>
2021-09-27 09:05:09 +0530

    Update and rename home.php to index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
-  header("location:login.php");
+  header("location:/login.php");
   exit;
 }
 
@@ -14,14 +14,14 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="/style.css">
   <title>Home</title>
 </head>
 
 <body>
 
   <?php
-  include 'Components/_navbar.php';
+  include '/Components/_navbar.php';
   ?>
   <!-- <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
